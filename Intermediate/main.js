b4d0e75076fd7b608559fdafece09f579c693569
KrishnaVamsi49 <56794078+KrishnaVamsi49@users.noreply.github.com>
2020-10-15 19:29:40 +0530

    Add files via upload

---
+++ Intermediate/main.js
@@ -0,0 +1,24 @@
+let mainAvatar = [
+    "avatar-anisha.png",
+    "avatar-ali.png",
+    "avatar-richard.png",
+  ];
+  let mainHeading = [
+    "Anisha Li",
+    "Ali Bravo",
+    "Richard Watts",
+  ];
+  let mainPara = [
+    ` “Manage has supercharged our team’s workflow. The ability to
+    maintain visibility on larger milestones at all times keeps
+    everyone motivated.”`,
+    `“We have been able to cancel so many other subscriptions since
+    using Manage. There is no more cross-channel confusion and
+    everyone is much more focused.”`,
+    `“Manage allows us to provide structure and process. It keeps us
+    organized and focused. I can’t stop recommending them to everyone
+    I talk to!”
+    `,
+  ];
+  
+  export { mainAvatar, mainHeading, mainPara };
